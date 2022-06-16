b5eb872b19b086774a435df9e47dde13afa0e474
theresagoldschmidte <s6thgold@uni-bonn.de>
2022-06-16 21:52:16 +0200

    basics

--- Calibration_script/main.py
+++ Calibration_script/main.py
@@ -21,15 +21,15 @@ PLOTS (0)-(2)___________________________________________________________________
 config.read("/Users/resi/PycharmProjects/pxd_teststand_software/Calibration_script/path.ini")
 config["calibration_data"].get("data_path")
 
-data_IvsI = pd.read_csv(config["calibration_data"].get("data_path"),sep=" ",header=None)
-data_IvsI.columns=["1","2","3","4","5","6"]# Number of columns you can see in the dat file.
+data_UvsU = pd.read_csv(config["calibration_data"].get("data_path"),sep=" ",header=None)
+data_UvsU.columns=["1","2","3","4","5","6"]# Number of columns you can see in the dat file.
 
 """
 0) Plot(U Cal: Uset vs. U out)
 """
 
-x_0 = data_IvsI['1']
-y_0 = data_IvsI['2']
+x_0 = data_UvsU['1']
+y_0 = data_UvsU['2']
 
 # calculating linear regression
 (m_0, b_0), (SSE,), *_ = np.polyfit(x_0, y_0, deg=1, full=True)
@@ -51,8 +51,8 @@ plt.show()
 1) U Cal: Uout vs. MonUreg
 """
 
-x_1 = data_IvsI['2']
-y_1 = data_IvsI['3']
+x_1 = data_UvsU['2']
+y_1 = data_UvsU['3']
 
 # calculating linear regression
 (m_1, b_1),  *_ = np.polyfit(x_1, y_1, deg=1, full=True)
@@ -68,6 +68,83 @@ plt.text(1000,200, f'slope = {round(m_1, 4)}, int= {round(b_1,4)}')
 print(f'ADC_U_LOAD_GAIN={round(m_1, 4)*10000} {os.linesep}ADC_U_LOAD_OFFSET={round(b_1,4)*100} ')
 plt.show()
 
+"""                                                                                               
+2) U Cal: Uout vs. MonUload                                                                        
+"""
+
+x_2 = data_UvsU['2']
+y_2 = data_UvsU['4']
+
+# calculating linear regression
+(m_2, b_2),  *_ = np.polyfit(x_2, y_2, deg=1, full=True)
+
+# plot
+plt.scatter(x_2,y_2, color='grey', marker='x')
+plt.plot(x_2, m_2 * x_2 + b_2, color='r' )
+plt.xlabel('$U_{out}$ [mV]')
+plt.ylabel('$U_{load}$ [mV]')
+plt.title('$U_{out} vs. U_{load}$')
+plt.text(1000,200, f'slope = {round(m_2, 4)}, int= {round(b_2,4)}')
+
+print(f'ADC_U_REGULATOR_GAIN={round(m_2, 4)*10000} {os.linesep}ADC_U_REGULATOR_={round(b_2,4)*100} ')
+plt.show()
+
+"""                                                                                                                 
+PLOTS (3)___________________________________________________________________________________________________        
+"""
+#still need a dynamic solution for the path stuff
+
+data_IvsI = pd.read_csv(config["calibration_data"].get("data_path_2"),sep=" ",header=None)
+data_IvsI.columns=["1","2","3","4","5","6"]# Number of columns you can see in the dat file.
+
+"""
+(3) I Cal: Iout vs. IoutMon
+"""
+
+x_3 = data_IvsI['2']
+y_3 = data_IvsI['3']
+
+# calculating linear regression
+(m_3, b_3), *_ = np.polyfit(x_3, y_3, deg=1, full=True)
+
+# plot
+plt.scatter(x_3,y_3, color='grey', marker='x')
+plt.plot(x_3, m_3 * x_3 + b_3, color='r' )
+plt.xlabel('$I_{dac}$ [mA]')
+plt.ylabel('$I_{outMon}$ [mA]')
+plt.title('$U_{set} vs. U_{out}$')
+plt.text(-1000,200, f'slope = {round(m_3, 4)}, int= {round(b_3,4)}')
+
+print(f'ADC_I_MON_GAIN={round(m_3, 4)*10000} {os.linesep}ADC_I_MON_OFFSET={round(b_3,2)*100} ')
+plt.show()
 
 
 
+""" 
+PLOTS (3)_______________________________________________________________________________________________data_IvsI = pd.read_csv(config["calibration_data"].get("data_path"),sep=" ",header=None)
+"""
+
+data_IvsI = pd.read_csv(config["calibration_data"].get("data_path_3"),sep=" ",header=None)
+data_IvsI.columns=["1","2","3","4","5"]# Number of columns you can see in the dat file.
+
+"""
+4) I Cal: DAC LIMIT vs. I Measured
+"""
+
+x_4 = data_IvsI['1']
+y_4 = data_IvsI['2']
+
+# calculating linear regression
+(m_4, b_4),  *_ = np.polyfit(x_4, y_4, deg=1, full=True)
+
+# plot
+plt.scatter(x_4,y_4, color='grey', marker='x')
+plt.plot(x_4, m_4 * x_4 + b_4, color='r' )
+plt.xlabel('$Limit DAC$ [mV]')
+plt.ylabel('$Limit Current$ [mA]')
+plt.title('$DAC LIMIT vs. I Measured$')
+plt.text(0,-1500, f'slope = {round(m_4, 4)}, int= {round(b_4,4)}')
+
+
+print(f'DAC_CURRENT_GAIN={round(m_4, 4)*10000} {os.linesep}DAC_CURRENT_OFFSET={round(b_4,4)*100}  ')
+plt.show()
