073f1e2a48fab5e25ded4228025a5bbe63e1052f
theresagoldschmidte <s6thgold@uni-bonn.de>
2022-07-01 16:15:59 +0200

    new solution for cutting outliers

--- Calibration_script/compare_ini.py
+++ Calibration_script/compare_ini.py
@@ -7,12 +7,18 @@ import csv
 config_old = configparser.ConfigParser()
 config_new = configparser.ConfigParser()
 
-x = []
+#config_old.read("/Users/resi/Desktop/Schreibtisch - MacBook Pro von Theresa/SS2022/BA scibo/CalibrationData/MainProdNODE18/constants_node_56.ini")
+
+
+config_old.read("/Users/resi/Desktop/Schreibtisch - MacBook Pro von Theresa/SS2022/BA scibo/CalibrationData/1_Calibration_ohneOVP_ps87/constants_node_56.ini")
+config_new.read("/Users/resi/PycharmProjects/pxd_teststand_software/Calibration_script/constants.ini")
+
+
 
 def gain_offset(name_gain, name_offset,channel):
     gain_old = float(config_old[f'{channel}'].get(name_gain))
     gain_new = float(config_new[f'{channel}'].get(name_gain))
-    gain_diff = round((np.abs(gain_old - gain_new) / gain_new) * 100, 2)
+    gain_diff = round(((gain_old - gain_new) / gain_new) * 100, 2)
     writer.writerow({'Channel': channel, 'Constants': name_gain, 'old': gain_old, 'new': gain_new,'difference': gain_diff, 'in': '%'})
 
     offset_old = float(config_old[f'{channel}'].get(name_offset))
@@ -20,9 +26,6 @@ def gain_offset(name_gain, name_offset,channel):
     offset_diff = offset_old - offset_new
     writer.writerow({'Channel': channel, 'Constants': name_offset, 'old': offset_old,'new': offset_new, 'difference': offset_diff, 'in': 'abs'})
 
-
-config_old.read("/Users/resi/Desktop/Schreibtisch - MacBook Pro von Theresa/SS2022/BA scibo/CalibrationData/1_Calibration_ohneOVP_ps87/constants_node_56.ini")
-config_new.read("/Users/resi/PycharmProjects/pxd_teststand_software/Calibration_script/constants.ini")
 
 
 
@@ -38,43 +41,77 @@ with open('comparison.csv', 'w',encoding='UTF8') as csvfile:
         gain_offset(f'ADC_I_MON_GAIN', f'ADC_I_MON_OFFSET', channel)
         gain_offset(f'DAC_CURRENT_GAIN', f'DAC_CURRENT_OFFSET', channel)
 
-    comp = pd.read_csv('comparison.csv', sep=",", header=0)
-    comp.columns = ['Channel', 'Constants', 'old', 'new', 'difference','in']
+    csvfile.close()
+
+def plot_diff():
+
+    with open('comparison.csv', 'r', encoding='UTF8') as csvfile:
+        comp = pd.read_csv('comparison.csv', sep=",", header=0)
+        comp.columns = ['Channel', 'Constants', 'old', 'new', 'difference', 'in']
+
+        # slope differences
+        difference = comp['difference']
+        voltage_gain = difference[::10]
+        load_gain = difference[2::10]
+        regulator_gain = difference[4::10]
+        mon_gain = difference[6::10]
+        current_gain = difference[8::10]
+
+        # for i in range (5):
+        #    channel_i = list(comp['Channel'] - 0.2 + (i*0.1))
+        #    channel_short_i = channel_i[i*2::10]
+
+        channel_1 = list(comp['Channel'])
+        channel_2 = list(comp['Channel'] + 0.2)
+        channel_3 = list(comp['Channel'] + 0.4)
+        channel_4 = list(comp['Channel'] + 0.6)
+        channel_5 = list(comp['Channel'] + 0.8)
+        channel_short_1 = channel_1[0::10]
+        channel_short_2 = channel_2[2::10]
+        channel_short_3 = channel_3[4::10]
+        channel_short_4 = channel_4[6::10]
+        channel_short_5 = channel_5[8::10]
+
+        plt.subplots(2,1,sharex='col')
+        plt.subplot(2, 1, 1)
+        plt.bar(channel_short_1, voltage_gain, color='b', width=0.2, label = 'Voltage')
+        plt.bar(channel_short_2, load_gain, color='r', width=0.2, label = 'load')
+        plt.bar(channel_short_3, regulator_gain, color='y', width=0.2, label = 'regulator')
+        plt.bar(channel_short_4, mon_gain, color='g', width=0.2, label = 'mon')
+        plt.bar(channel_short_5, current_gain, color='pink', width=0.2, label = 'current')
+        # plt.grid()
+        #plt.xticks(np.arange(0, 24, 1))
+        #plt.xlabel("Channel")
+        plt.legend(prop={'size':6})
+        plt.ylabel("percentage gain difference")
+
+        # gain differences
+        difference = comp['difference']
+        voltage_offset = difference[1::10]
+        load_offset = difference[3::10]
+        regulator_offset = difference[5::10]
+        mon_offset = difference[7::10]
+        current_offset = difference[9::10]
+
+
+        plt.subplot(2, 1, 2)
+        plt.bar(channel_short_1, voltage_offset, color='b', width=0.2)
+        plt.bar(channel_short_2, load_offset, color='r', width=0.2)
+        plt.bar(channel_short_3, regulator_offset, color='y', width=0.2)
+        plt.bar(channel_short_4, mon_offset, color='g', width=0.2)
+        plt.bar(channel_short_5, current_offset, color='pink', width=0.2)
+        plt.xticks(np.arange(0, 24, 1))
+        plt.xlabel("Channel")
+        plt.ylabel("absolut offset difference")
+
+        plt.savefig("comparison.pdf", format='pdf', bbox_inches='tight')
+        plt.close()
 
 
 
-    difference = comp['difference']
-    voltage_gain = difference[::10]
-    load_gain = difference[2::10]
-    regulator_gain = difference[4::10]
-    mon_gain = difference[6::10]
-    current_gain = difference[8::10]
+def main ():
+    plot_diff()
 
 
-    channel_1 = list(comp['Channel']-0.2)
-    channel_2 = list(comp['Channel']-0.1)
-    channel_3 = list(comp['Channel'])
-    channel_4 = list(comp['Channel'] + 0.1)
-    channel_5 = list(comp['Channel'] + 0.2)
-    channel_short_1 = channel_1[0::10]
-    channel_short_2 = channel_2[2::10]
-    channel_short_3 = channel_3[4::10]
-    channel_short_4 = channel_4[6::10]
-    channel_short_5 = channel_5[8::10]
-
-
-    #for i in range(5):
-    #    channel_i = list(comp['Channel']-0.2+(i*0.1))
-    #    channel_short_i = channel_i[i*2::10]
-    #    plt.bar(channel_short_i, voltage_gain, color='b', width=0.1)
-
-    plt.bar(channel_short_1, voltage_gain, color='b', width=0.1)
-    plt.bar(channel_short_2, load_gain, color = 'r', width = 0.1)
-    plt.bar(channel_short_3, regulator_gain, color='y', width=0.1)
-    plt.bar(channel_short_4, mon_gain, color='g', width=0.1)
-    plt.bar(channel_short_5, current_gain, color='pink', width=0.1)
-    #plt.grid()
-    plt.xticks(np.arange(0,24,1))
-    plt.show()
-    plt.close()
-
+if __name__ == '__main__':
+    main()
--- Calibration_script/main.py
+++ Calibration_script/main.py
@@ -2,77 +2,124 @@ import numpy as np
 import matplotlib.pyplot as plt
 import configparser
 import os
+from Calibration_script import fit
+
 
 config = configparser.ConfigParser()
 config_ini = configparser.ConfigParser()
 config_ini.optionxform = str
-from Calibration_script import fit
-np.seterr(divide='ignore', invalid='ignore')
 
 
 
-
-def cut_outliers(x, y):
+def prepare_data(x, y):
     """
-    :param slopes: np array
+    Excluding none and inf in data
+    :param x: calibration data
+    :param y: calibration data
+    :return: calibration data without nan and inf
+    """
+    cut = np.zeros_like(x, dtype=bool)
+    cut[np.isinf(x) | np.isnan(x)] = True
+    cut[np.isinf(y) | np.isnan(y)] = True
+    cut[x == 0] = True
+    return x[~cut], y[~cut]
+
+
+def cut_outliers(x, y, channel):
+    """
+    :param x: np array
     :return: cut data
     """
+    # Calculating the slope
+    slopes = (y - y[0])/x
+    m = np.polyfit(x[:20],y[:20],deg = 1)
 
-    slopes = y / x
+    tolerance = abs(y[0] - y[-1])*0.01
+    # Making array same size as data with only False in it
     cut = np.zeros_like(slopes, dtype=bool)
-    cut[np.isinf(slopes) | np.isnan(slopes)] = True
-    cut[1:][np.isclose(np.diff(y), 0, atol = 1E-02)] = True
+    # Set False to zero in parts where data gradient is close to zero
+    if channel == 13:
+        cut[:][np.isclose(np.gradient(y), 0, atol=tolerance)] = True
+
+    else:
+        cut[:][np.isclose(np.gradient(y), 0, atol=1)] = True
+        cut[:][abs((m[0]*x+m[1])-y)> tolerance] = True
+
+    # calculate mean and standard derivation
     mean, std = np.mean(slopes[~cut]), np.std(slopes[~cut])
+    # cut to sigma (95% stays)
     cut[np.logical_or((slopes > 2 * std + mean), (slopes < mean - 2 * std))] = True
 
-    #print(np.count_nonzero(~cut) / len(slopes) * 100.)
-    return x[~cut], y[~cut]
+    return x[~cut], y[~cut],x[cut], y[cut]
 
 
 
 def linear(m,x,b):
+    """
+    Defining the form of a linear function
+    :param m: slope
+    :param x: calibration data
+    :param b: offset
+    :return: returns form of linear function
+    """
     return m*x+b
 
-def plot_and_fit(x, y,dx, dy, xlabel, ylabel, label):
+
+def plot_and_fit(x, y, dx, dy, x_cut, y_cut, xlabel, ylabel, label):
     """
-    :param x: Calibration data
+    Creates plots from cleaned calibration data and linear regression
+    :param x: Calibration Data
     :param y: Calibration Data
-    :return: slope and offset
+    :param dx: error
+    :param dy: error
+    :param xlabel: Label of x axis
+    :param ylabel:  label of y axis
+    :param label: title of plot
+    :return: plot, slope and offset
     """
 
-    #(m, b), (SSE,), *_ = np.polyfit(x, y, deg=1, full=True)
+    # p0 estimator
+    p0 = np.mean((y-y[0])/x)
+
+    # (m, b), (SSE,), *_ = np.polyfit(x, y, deg=1, full=True)
     popt, perr, red_chi_2 = fit.fit_odr(fit_func=linear,
                                         x=x,
                                         y=y,
                                         x_err=dx,
                                         y_err=dy,
-                                        p0=[1.,2.])
+                                        p0=[p0,y[0]])
 
     # Make result string
-    res = "Fit results of {}:\n\t".format(fit.fit_odr.__name__)
-    res += "m = ({:.3E} {} {:.3E})".format(popt[0], u'\u00B1', perr[0]) + " keV\n\t"
-    res += "b = ({:.3E} {} {:.3E})".format(popt[1], u'\u00B1', perr[1]) + " keV\n\t"
+    res = "Fit results of {}:\n".format(fit.fit_odr.__name__)
+    res += "m = ({:.3E} {} {:.3E})".format(popt[0], u'\u00B1', perr[0]) + " \n"
+    res += "b = ({:.3E} {} {:.3E})".format(popt[1], u'\u00B1', perr[1]) + " \n"
     res += "red. Chi^2 = {:.3f}".format(red_chi_2) + "\n"
     m = popt[0]
     b = popt[1]
 
-
-    # plot
-    plt.scatter(x, y, color='grey', marker='.', label=label, linewidths=1.0)
-    plt.errorbar(x, y, yerr=dy, xerr=dx, fmt='none', ecolor='grey', ms=1)
-    plt.plot(x, m * x + b, color='r', label=f'slope = {round(m, 4)}, int= {round(b, 4)}''\n'f' $\chi^2$ = {round(red_chi_2, 4)}')
+    # plot each of 5 subplots
+    plt.scatter(x, y, color='k', marker='.', linewidths=1.0 )
+    plt.scatter(x_cut, y_cut, color='grey',marker='.', linewidths=1.0, label = 'Outliers')
+    plt.errorbar(x, y, yerr=dy, xerr=dx, fmt='none', ecolor='k', label = label)
+    plt.plot(x, m * x + b, color='r', label=f'slope = {round(m, 4)}''\n' f'int= {round(b, 4)}''\n'r' $\chi^2_{\mathrm{red}}$ =' f'{round(red_chi_2, 4)}')
     plt.rcParams["figure.autolayout"] = True
     plt.xlabel(xlabel)
     plt.ylabel(ylabel)
     plt.legend(prop={'size':8})
 
-    return (m, b)
+    return m, b
+
 
 def read_data(path, columns):
+    """
+    Gets data from path
+    :param path: path to .dat diles
+    :param columns: multiple columns in each file
+    :return: data
+    """
     dtype = [(col, '<f8') for col in columns]
     data = np.loadtxt(fname=path, delimiter=" ", skiprows=0, dtype=dtype)
     return data
-
 
 
 def main():
@@ -90,39 +137,37 @@ def main():
         """
         plt.subplots(figsize=(12, 6))
 
-
-
         # Opening the U_vs_U ini file
         path_UvsU = os.path.join(config["calibration_data"].get("data_path"), 'Channel_%d_U_vs_U' % channel + '.dat')
 
-        columns_UvsU = ["$U_{dac}$ [mV]", "$U_{out}$ [mV]", "$U_{regulator}$ [mV]", "$U_{load}$ [mV]", "unknown 5",
-                             "unknown 6"]
+        columns_UvsU = ["$U_{dac}$ [mV]", "$U_{out}$ [mV]", "$U_{regulator}$ [mV]", "$U_{load}$ [mV]", "unknown 5","unknown 6"]
 
         data_UvsU = read_data(path_UvsU, columns_UvsU)
-
-
-
 
         # 0) Plot(U Cal: Uset vs. U out)
         x_0 = data_UvsU['$U_{dac}$ [mV]']
         y_0 = data_UvsU['$U_{out}$ [mV]']
-        x_0, y_0 = cut_outliers(x_0, y_0)
+        x_0,y_0 = prepare_data(x_0, y_0)
+        x_0, y_0,x_cut_0,y_cut_0 = cut_outliers(x_0, y_0, channel)
+
         plt.subplot(2, 3, 1)
-        m_0, b_0 = plot_and_fit(x_0, y_0, 10, 10, '$U_{dac}$ [mV]', '$U_{out}$ [mV]', '$U_{set} vs. U_{out}$')
+        m_0, b_0 = plot_and_fit(x_0, y_0, 10, 10, x_cut_0,y_cut_0,  '$U_{dac}$ [mV]', '$U_{out}$ [mV]', '$U_{set} vs. U_{out}$')
 
         # 1) U Cal: Uout vs. MonUreg
         x_1 = data_UvsU['$U_{out}$ [mV]']
         y_1 = data_UvsU['$U_{regulator}$ [mV]']
-        x_1, y_1 = cut_outliers(x_1, y_1)
+        x_1, y_1 = prepare_data(x_1, y_1)
+        x_1, y_1,x_cut_1,y_cut_1 = cut_outliers(x_1, y_1, channel)
         plt.subplot(2, 3, 2)
-        m_1, b_1 = plot_and_fit(x_1, y_1, 10, 10, '$U_{out}$ [mV]', '$U_{regulator}$ [mV]', '$U_{out} vs. U_{regulator}$')
+        m_1, b_1 = plot_and_fit(x_1, y_1, 10, 10 ,x_cut_1,y_cut_1, '$U_{out}$ [mV]', '$U_{regulator}$ [mV]', '$U_{out} vs. U_{regulator}$')
 
         # 2) U Cal: Uout vs. MonUload
         x_2 = data_UvsU['$U_{out}$ [mV]']
         y_2 = data_UvsU['$U_{load}$ [mV]']
-        x_2, y_2 = cut_outliers(x_2, y_2)
+        x_2, y_2 = prepare_data(x_2, y_2)
+        x_2, y_2, x_cut_2,y_cut_2 = cut_outliers(x_2, y_2, channel)
         plt.subplot(2, 3, 3)
-        m_2, b_2 = plot_and_fit(x_2, y_2, 10, 10, '$U_{out}$ [mV]', '$U_{load}$ [mV]', '$U_{out} vs. U_{load}$')
+        m_2, b_2 = plot_and_fit(x_2, y_2, 10, 10, x_cut_2,y_cut_2, '$U_{out}$ [mV]', '$U_{load}$ [mV]', '$U_{out} vs. U_{load}$')
 
         # changing to I_vs_I.dat file 'Channel_%d_I_vs_I'%channel
 
@@ -131,16 +176,17 @@ def main():
 
         data_IvsI = read_data(path_IvsI, columns_IvsI)
 
-
         # (3) I Cal: Iout vs. IoutMon
         x_3 = data_IvsI['$I_{dac}$ [mA]']
         y_3 = data_IvsI['$I_{outMon}$ [mA]']
-        x_3, y_3 = cut_outliers(x_3, y_3)
+        x_3, y_3 = prepare_data(x_3, y_3)
+        x_3, y_3, x_cut_3,y_cut_3= cut_outliers(x_3, y_3, channel)
         plt.subplot(2, 3, 4)
         if (channel == 13):
-            m_3, b_3 = plot_and_fit(x_3, y_3, 0.001, 0.001,'$I_{dac}$ [mA]', '$I_{outMon}$ [$mu$A]', '$I_{out} vs. I_{outMon}$')
+            m_3, b_3 = plot_and_fit(x_3, y_3, 0.001, 0.001,x_cut_3,y_cut_3,'$I_{dac}$ [mA]', '$I_{outMon}$ [$mu$A]', '$I_{out} vs. I_{outMon}$')
+            m_3 = m_3/1000
         else:
-            m_3, b_3 = plot_and_fit(x_3, y_3, 1, 1, '$I_{dac}$ [mA]', '$I_{outMon}$ [mA]', '$I_{out} vs. I_{outMon}$')
+            m_3, b_3 = plot_and_fit(x_3, y_3, 1, 1, x_cut_3,y_cut_3, '$I_{dac}$ [mA]', '$I_{outMon}$ [mA]', '$I_{out} vs. I_{outMon}$')
 
         # changing to Ilimit_vs_I.dat file
         path_IlimitvsI = os.path.join(config["calibration_data"].get("data_path"),'Channel_%d_Ilimit_vs_I' % channel + '.dat')
@@ -153,14 +199,13 @@ def main():
         # 4) I Cal: DAC LIMIT vs. I Measured
         x_4 = data_IlimitvsI['$Limit DAC$ [mV]']
         y_4 = data_IlimitvsI['$Limit Current$ [mA]']
-        x_4, y_4 = cut_outliers(x_4, y_4)
+        x_4, y_4 = prepare_data(x_4, y_4)
+        x_4, y_4, x_cut_4,y_cut_4 = cut_outliers(x_4, y_4, channel)
         plt.subplot(2, 3, 5)
-        m_4, b_4 = plot_and_fit(x_4, y_4, 10, 1, '$Limit DAC$ [mV]', '$Limit Current$ [mA]', '$DAC LIMIT vs. I Measured$')
-        if (channel == 13):
-            m_4 = m_4 *1000
-            b_4 = b_4 *1000
-
-
+        m_4, b_4 = plot_and_fit(x_4, y_4, 10, 1, x_cut_4,y_cut_4, '$Limit DAC$ [mV]', '$Limit Current$ [mA]', '$DAC LIMIT vs. I_{Measured}$')
+        if channel == 13:
+            m_4 = m_4 * 1000
+            b_4 = b_4 * 1000
 
         # All 5 plots in one figure
         plt.subplots_adjust(left=0.1,
@@ -189,7 +234,6 @@ def main():
 
     with open('constants.ini', 'w') as configfile:
         config_ini.write(configfile)
-
 
 
 if __name__ == '__main__':
