fix in main - new calculation for tolerance

Calibration_script/compare_ini.py
@@ -28,7 +28,7 @@ def gain_offset(name_gain, name_offset,channel):
 
     offset_old = float(config_old[f'{channel}'].get(name_offset))
     offset_new = float(config_new[f'{channel}'].get(name_offset))
-    offset_diff = (offset_old - offset_new)
+    offset_diff = round(((offset_old - offset_new)/gain_new)*100, 2)
     writer.writerow({'Channel': channel, 'Constants': name_offset, 'old': offset_old,'new': offset_new, 'difference': offset_diff, 'in': 'abs'})
 
 
@@ -87,7 +87,7 @@ def plot_diff():
         plt.xticks(np.arange(0, 24, 1))
         #plt.xlabel("Channel")
         plt.legend(prop={'size':6})
-        plt.ylabel("Relative gain difference ")
+        plt.ylabel("Relative gain difference [%]", fontsize=6)
 
         # gain differences
         difference = comp['difference']
@@ -106,7 +106,7 @@ def plot_diff():
         plt.bar(channel_short_5, current_offset, color='pink', width=0.2)
         plt.xticks(np.arange(0, 24, 1))
         plt.xlabel("Channel")
-        plt.ylabel("Absolute offset difference")
+        plt.ylabel("Offset diff. relative to gain  [%]", fontsize=6)
         plt.grid(color='grey', linestyle='--', linewidth=0.1)
         plt.savefig(save_to+"comparison.pdf", format='pdf', bbox_inches='tight')
         plt.close()

Calibration_script/constant_stability.py
@@ -4,11 +4,11 @@ import csv
 import numpy as np
 
 
-path = '/Users/resi/Desktop/Schreibtisch - MacBook Pro von Theresa/SS2022/BA scibo/Calibrations/ps15/output.csv'
-errors = '/Users/resi/Desktop/Schreibtisch - MacBook Pro von Theresa/SS2022/BA scibo/Calibrations/ps15/output_err.csv'
-path_pdf = '/Users/resi/Desktop/Schreibtisch - MacBook Pro von Theresa/SS2022/BA scibo/Calibrations/ps15/Calibrationskonstanten_pdf_ps15.pdf'
-path_var = '/Users/resi/Desktop/Schreibtisch - MacBook Pro von Theresa/SS2022/BA scibo/Calibrations/ps15/Calibrationskonstanten_var_pdf_ps15.pdf'
-
+path = '/Users/resi/Desktop/Schreibtisch - MacBook Pro von Theresa/SS2022/BA scibo/Calibrations/ps26/output.csv'
+errors = '/Users/resi/Desktop/Schreibtisch - MacBook Pro von Theresa/SS2022/BA scibo/Calibrations/ps26/output_err.csv'
+path_pdf = '/Users/resi/Desktop/Schreibtisch - MacBook Pro von Theresa/SS2022/BA scibo/Calibrations/ps26/Calibrationskonstanten_pdf_ps26.pdf'
+path_var = '/Users/resi/Desktop/Schreibtisch - MacBook Pro von Theresa/SS2022/BA scibo/Calibrations/ps26/Calibrationskonstanten_var_pdf_ps26.pdf'
+#path_old = '/Users/resi/Desktop/Schreibtisch - MacBook Pro von Theresa/SS2022/BA scibo/Calibrations/ps26/Calibrationskonstanten_old_pdf_ps.pdf'
 
 def plot_data(channel):
     i = 10 * channel + channel
@@ -41,33 +41,35 @@ def plot_data(channel):
             DAC_CURRENT_GAIN_ERROR = rows[i + 9]
             DAC_CURRENT_OFFSET_ERROR = rows[i + 10]
 
-            var_g_1 = plot(run[1:],DAC_VOLTAGE_GAIN[1:],DAC_VOLTAGE_GAIN_ERROR[1:], 'mV/mV', 'lightcoral', 'DAC_VOLTAGE_GAIN', 1)
-            var_o_1 = plot(run[1:], DAC_VOLTAGE_OFFSET[1:],DAC_VOLTAGE_OFFSET_ERROR[1:], 'mV', 'darkgrey', 'DAC_VOLTAGE_OFFSET', 2)
-            var_g_2 = plot(run[1:], ADC_U_LOAD_GAIN[1:],ADC_U_LOAD_GAIN_ERROR[1:], 'mV/mV', 'orange', 'ADC_U_LOAD_GAIN', 3)
-            var_o_2 = plot(run[1:], ADC_U_LOAD_OFFSET[1:],ADC_U_LOAD_OFFSET_ERROR[1:], 'mV', 'darkseagreen', 'ADC_U_LOAD_OFFSET', 4)
-            var_g_3 = plot(run[1:], ADC_U_REGULATOR_GAIN[1:],ADC_U_REGULATOR_GAIN_ERROR[1:], 'mV/mV', 'cadetblue', 'ADC_U_REGULATOR_GAIN', 5)
-            var_o_3 = plot(run[1:], ADC_U_REGULATOR_OFFSET[1:],ADC_U_REGULATOR_OFFSET_ERROR[1:], 'mV', 'cornflowerblue', 'ADC_U_REGULATOR_OFFSET', 6)
-            var_g_4 = plot(run[1:], ADC_I_MON_GAIN[1:],ADC_I_MON_GAIN_ERROR[1:], 'mA/mV', 'slateblue', 'ADC_I_MON_GAIN', 7)
-            var_o_4 = plot(run[1:], ADC_I_MON_OFFSET[1:],ADC_I_MON_OFFSET_ERROR[1:], 'mV', 'thistle', 'ADC_I_MON_OFFSET', 8)
-            var_g_5 = plot(run[1:], DAC_CURRENT_GAIN[1:],DAC_CURRENT_GAIN_ERROR[1:], 'mA/mV', 'plum', 'DAC_CURRENT_GAIN', 9)
-            var_o_5 = plot(run[1:], DAC_CURRENT_OFFSET[1:],DAC_CURRENT_OFFSET_ERROR[1:], 'mA', 'palevioletred', 'DAC_CURRENT_OFFSET', 10)
+            var_g_1, abw_g_1 = plot(run[1:],DAC_VOLTAGE_GAIN[1:],DAC_VOLTAGE_GAIN_ERROR[1:], 'mV/mV', 'lightcoral', 'DAC_VOLTAGE_GAIN', 1)
+            var_o_1, abw_o_1 = plot(run[1:], DAC_VOLTAGE_OFFSET[1:],DAC_VOLTAGE_OFFSET_ERROR[1:], 'mV', 'darkgrey', 'DAC_VOLTAGE_OFFSET', 2)
+            var_g_2, abw_g_2 = plot(run[1:], ADC_U_LOAD_GAIN[1:],ADC_U_LOAD_GAIN_ERROR[1:], 'mV/mV', 'orange', 'ADC_U_LOAD_GAIN', 3)
+            var_o_2, abw_o_2 = plot(run[1:], ADC_U_LOAD_OFFSET[1:],ADC_U_LOAD_OFFSET_ERROR[1:], 'mV', 'darkseagreen', 'ADC_U_LOAD_OFFSET', 4)
+            var_g_3, abw_g_3 = plot(run[1:], ADC_U_REGULATOR_GAIN[1:],ADC_U_REGULATOR_GAIN_ERROR[1:], 'mV/mV', 'cadetblue', 'ADC_U_REGULATOR_GAIN', 5)
+            var_o_3, abw_o_3 = plot(run[1:], ADC_U_REGULATOR_OFFSET[1:],ADC_U_REGULATOR_OFFSET_ERROR[1:], 'mV', 'cornflowerblue', 'ADC_U_REGULATOR_OFFSET', 6)
+            var_g_4, abw_g_4 = plot(run[1:], ADC_I_MON_GAIN[1:],ADC_I_MON_GAIN_ERROR[1:], 'mA/mV', 'slateblue', 'ADC_I_MON_GAIN', 7)
+            var_o_4, abw_o_4 = plot(run[1:], ADC_I_MON_OFFSET[1:],ADC_I_MON_OFFSET_ERROR[1:], 'mV', 'thistle', 'ADC_I_MON_OFFSET', 8)
+            var_g_5, abw_g_5 = plot(run[1:], DAC_CURRENT_GAIN[1:],DAC_CURRENT_GAIN_ERROR[1:], 'mA/mV', 'plum', 'DAC_CURRENT_GAIN', 9)
+            var_o_5, abw_o_5 = plot(run[1:], DAC_CURRENT_OFFSET[1:],DAC_CURRENT_OFFSET_ERROR[1:], 'mA', 'palevioletred', 'DAC_CURRENT_OFFSET', 10)
 
-            return(var_g_1, var_o_1, var_g_2,var_o_2, var_g_3, var_o_3, var_g_4, var_o_4, var_g_5, var_o_5)
+            return(var_g_1, var_o_1, var_g_2,var_o_2, var_g_3, var_o_3, var_g_4, var_o_4, var_g_5, var_o_5, abw_g_1,abw_o_1,abw_g_2,abw_o_2,abw_g_3,abw_o_3,abw_g_4,abw_o_4,abw_g_5,abw_o_5)
 
 
 def plot(x_data, y_data,err, y_ax, color, title, n):
     plt.subplot(10, 1, n)
     y = [eval(i) for i in y_data]
     y_err = [eval(i) for i in err]
-
-    mean = np.mean(y)
-    std = np.std(y)
-
-
+    mean = np.mean(y[1:])
+    std = np.std(y[1:])
     if mean == 0:
         var = 0
     else:
         var = abs(std / mean)
+
+    if (y[0]) == 0 or mean == 0:
+        abw = 0
+    else:
+        abw = ((y[0] - mean) / mean) * 100
 
     stdb = std+std
     plotmean = []
@@ -78,10 +80,12 @@ def plot(x_data, y_data,err, y_ax, color, title, n):
     plt.plot(x_data,plotmean,'-',color=color,linewidth=3.0,label='median')
     plt.fill_between(x_data,plotmean+stdb,plotmean-stdb,  alpha=0.2, color=color, label='2$\sigma$')
     plt.errorbar(x_data, y, yerr=y_err, fmt='none', ecolor='grey')
-    plt.ylabel(y_ax)
+    plt.xticks(fontsize=16)
+    plt.xticks(fontsize=14)
+    plt.ylabel(y_ax, fontsize =14)
     plt.legend(prop={'size': 15})
 
-    return var
+    return (var, abw)
 
 
 def plot_histo(x,y,title,color):
@@ -91,6 +95,15 @@ def plot_histo(x,y,title,color):
     plt.xlabel('Channel')
     plt.xticks(np.arange(0, 24, 1))
     plt.ylabel('Variation coefficient')
+    plt.legend(prop={'size': 8})
+
+def plot_histo_abw(x,y,title,color):
+
+
+    plt.bar(x, y, color=color, label=title)
+    plt.xlabel('Channel')
+    plt.xticks(np.arange(0, 24, 1))
+    plt.ylabel('Relative difference of constant to mean [%]')
     plt.legend(prop={'size': 8})
 
 def main():
@@ -109,10 +122,21 @@ def main():
         l_var_g_5 = []
         l_var_o_5 = []
 
+        ol_var_g_1 = []
+        ol_var_o_1 = []
+        ol_var_g_2 = []
+        ol_var_o_2 = []
+        ol_var_g_3 = []
+        ol_var_o_3 = []
+        ol_var_g_4 = []
+        ol_var_o_4 = []
+        ol_var_g_5 = []
+        ol_var_o_5 = []
+
         for channel in range(24):
             print('Plotting Channel %d' %channel)
             plt.subplots(figsize=(20, 30))
-            (var_g_1, var_o_1, var_g_2,var_o_2, var_g_3, var_o_3, var_g_4, var_o_4, var_g_5, var_o_5) = plot_data(channel)
+            (var_g_1, var_o_1, var_g_2,var_o_2, var_g_3, var_o_3, var_g_4, var_o_4, var_g_5, var_o_5, abw_g_1,abw_o_1,abw_g_2,abw_o_2,abw_g_3,abw_o_3,abw_g_4,abw_o_4,abw_g_5,abw_o_5) = plot_data(channel)
             plt.suptitle('Channel %d\n'%channel, size=40.0)
             plt.xlabel('\n Date of Calibration Run', size=25.0)
             plt.tight_layout()
@@ -128,6 +152,19 @@ def main():
             l_var_o_4.append(var_o_4)
             l_var_g_5.append(var_g_5)
             l_var_o_5.append(var_o_5)
+
+            ol_var_g_1.append(abw_g_1)
+            ol_var_o_1.append(abw_o_1)
+            ol_var_g_2.append(abw_g_2)
+            ol_var_o_2.append(abw_o_2)
+            ol_var_g_3.append(abw_g_3)
+            ol_var_o_3.append(abw_o_3)
+            ol_var_g_4.append(abw_g_4)
+            ol_var_o_4.append(abw_o_4)
+            ol_var_g_5.append(abw_g_5)
+            ol_var_o_5.append(abw_o_5)
+
+
 
         with PdfPages(path_var) as pdf:
             plot_histo(channel_histo, l_var_g_1,  'DAC_VOLTAGE_GAIN', 'lightcoral')
@@ -161,8 +198,38 @@ def main():
             pdf.savefig()
             plt.close()
 
-
-
+        #with PdfPages(path_old) as pdf:
+        #    plot_histo_abw(channel_histo, ol_var_g_1,  'DAC_VOLTAGE_GAIN', 'lightcoral')
+        #    pdf.savefig()
+        #    plt.close()
+        #    plot_histo_abw(channel_histo, ol_var_o_1,  'DAC_VOLTAGE_OFFSET', 'darkgrey')
+        #    pdf.savefig()
+        #    plt.close()
+        #    plot_histo_abw(channel_histo, ol_var_g_2, 'ADC_U_LOAD_GAIN', 'orange')
+        #    pdf.savefig()
+        #    plt.close()
+        #    plot_histo_abw(channel_histo, ol_var_o_2, 'ADC_U_LOAD_OFFSET', 'darkseagreen')
+        #    pdf.savefig()
+        #    plt.close()
+        #    plot_histo_abw(channel_histo, ol_var_g_3, 'ADC_U_REGULATOR_GAIN', 'cadetblue')
+        #    pdf.savefig()
+        #    plt.close()
+        #    plot_histo_abw(channel_histo, ol_var_o_3, 'ADC_U_REGULATOR_OFFSET', 'cornflowerblue')
+        #    pdf.savefig()
+        #    plt.close()
+        #    plot_histo_abw(channel_histo, ol_var_g_4, 'ADC_I_MON_GAIN', 'slateblue')
+        #    pdf.savefig()
+        #    plt.close()
+        #    plot_histo_abw(channel_histo, ol_var_o_4, 'ADC_I_MON_OFFSET', 'thistle')
+        #    pdf.savefig()
+        #    plt.close()
+        #    plot_histo_abw(channel_histo, ol_var_g_5, 'DAC_CURRENT_GAIN', 'plum')
+        #    pdf.savefig()
+        #    plt.close()
+        #    plot_histo_abw(channel_histo, ol_var_o_5, 'DAC_CURRENT_OFFSET', 'palevioletred')
+        #    pdf.savefig()
+        #    plt.close()
+        #    print('done')
 
 
 

Calibration_script/main.py
@@ -43,7 +43,11 @@ def cut_outliers(x, y, channel):
     slopes = (y - y[0])/x
     m = np.polyfit(x[:20],y[:20],deg = 1)
 
-    tolerance = abs(y[0] - y[-1])*0.01
+    #tolerance = abs(y[0] - y[-2])*0.01
+
+    tolerance = abs(m[0]*x[0] - m[0]*x[-1])*0.01
+
+
 
     # Making array same size as data with only False in it
     cut = np.zeros_like(slopes, dtype=bool)
@@ -54,6 +58,7 @@ def cut_outliers(x, y, channel):
     else:
 
         cut[:][np.isclose(np.gradient(y), 0, atol=tolerance)] = True
+
         #cut[:][(abs(m[0]*x+m[1]))-y >= tolerance] = True
 
     if x[~cut].size == 0: